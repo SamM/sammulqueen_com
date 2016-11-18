f3c1bd24cca761332aa2d940dae578508e03f873
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2016-11-19 12:03:12 +1300

    Updated Server class to load listeners from the /server/listeners folder.
    Loads all js or json files from every folder inside /server/listeners.
    Each file must return an array of listeners to be concatenated on to the
    end of the events.listeners array.

--- server/Server.js
+++ server/Server.js
@@ -1,8 +1,29 @@
 const Events = require("../lib/Events.js");
+const fs = require('fs');
 
 const Server = function(port, hostname){
     const server = this;
     server.events = new Events();
+    // Load all listeners from /server/listeners folder
+    const path = "./listeners"
+    function loadListenersFolder(path){
+      fs.readdir(path, function(err, files){
+        if(!err){
+          for(var f=0;f<files.length;f++){
+            if(files[f].indexOf(".")<0){
+              // Directory
+              loadListenersFolder(path+"/"+files[f]);
+            }else if(files[f].indexOf(".json")>-1||files[f].indexOf(".js")>-1){
+              server.events.listeners = server.events.listeners.concat(require(path+"/"+files[f]));
+            }
+          }
+        }
+      });
+    }
+    server.loadListeners = function(){
+      server.events.listeners = [];
+      loadListenersFolder("./listeners");
+    }
 }
 
 module.exports = Server;
